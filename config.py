3a341c1480beac87e9dc76b3ddb8e766971e977c
AndreyGruzdov <andrj169@gmail.com>
2022-11-02 15:09:03 +0500

    init

---
+++ config.py
@@ -0,0 +1,47 @@
+valid_email = 'andrej169@yandex.ru'
+valid_password = 'K42mbNVrZBgCsyA'
+valid_pone = '+79522840568'
+
+url_rostelecom = 'https://b2c.passport.rt.ru/'
+url_cod = 'https://my.rt.ru/'
+url_email = "https://www.1secmail.com/api/v1/"
+
+import random
+
+engl = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+domens = ['com','ua','ru','net']
+
+
+def pass_gener():
+    passw_random = ''
+    for x in range(6):
+        passw_random = ''.join([passw_random,random.choice(list(engl))])
+
+    return passw_random.lower()
+
+
+def email_gener():
+    name_email = ''
+    name_server = ''
+    for x in range(6):
+        name_email = ''.join([name_email,random.choice(engl)])
+    for y in range(4):
+        name_server = ''.join([name_server,random.choice(engl)])
+
+    def domen():
+        dom = random.choice(domens)
+        return dom
+
+    email = ''.join([name_email,'@',name_server,'.',domen()])
+    return email.lower()
+
+
+fcv = ''
+for x in range(6):
+    fcv = fcv + random.choice(list(engl))
+
+login_no_valid = fcv
+ls_no_valid = fcv
+
+if __name__  == '__main__':
+    pass
---
+++ tests/test_authorization.py
@@ -0,0 +1,64 @@
+#python -m pytest -v --driver Chrome --driver-path chromedriver.exe tests\test_authorization.py
+#py -m pytest -v --driver Chrome --driver-path driver.exe
+#python -m pytest -v --driver Chrome --driver-path C:\Skillfactory\Drivers/chromedriver.exe
+
+import time
+from pages.auth_page import AuthPage
+from config import valid_email, valid_password, valid_pone, login_no_valid, ls_no_valid ,pass_gener, email_gener
+
+def test_main_page_all_items(web_browser):
+    """Проверка главной страницы"""
+    page = AuthPage(web_browser)
+    assert page.text_authorization.is_visible()
+    assert page.text_personal_area.is_visible()
+    assert page.email_input_form.is_visible()
+    assert page.password_input_form.is_visible()
+    assert page.tab_phone.is_visible()
+    assert page.tab_email.is_visible()
+    assert page.tab_login.is_visible()
+    assert page.tab_ls.is_visible()
+    assert 'https://b2c.passport.rt.ru/' in page.get_current_url()
+
+def test_positive_authorisation_phone(web_browser):
+    """Авторизация по номеру телефона и паролю"""
+    page = AuthPage(web_browser)
+    page.tab_phone.click()
+    page.email_input_form.send_keys(valid_pone)
+    page.password_input_form.send_keys(valid_password)
+    page.check_mark.click() #Снимаем галочку с поля запомнить меня
+    page.btn.click()
+    assert 'https://b2c.passport.rt.ru/account_b2c/page' in page.get_current_url()
+
+
+def test_positive_authorisation_email(web_browser):
+    """Авторизация по email и паролю"""
+    page = AuthPage(web_browser)
+    page.tab_email.click()
+    page.email_input_form.send_keys(valid_email)
+    page.password_input_form.send_keys(valid_password)
+    page.check_mark.click()
+    page.btn.click()
+    assert 'https://b2c.passport.rt.ru/account_b2c/page' in page.get_current_url()
+
+def test_negative_authorization_login(web_browser):
+    """Не удачная авторизация по логину и паролю"""
+    page = AuthPage(web_browser)
+    page.tab_login.click()
+    page.email_input_form.send_keys(login_no_valid)
+    page.password_input_form.send_keys(valid_password)
+    page.check_mark.click()
+    page.btn.click()
+    assert page.forgot_password.is_visible()
+    assert page.invalid_login_password.is_visible()
+
+def test_negative_authorization_ls(web_browser):
+    """Не удачная авторизация по лицевому счету и паролю"""
+    page = AuthPage(web_browser)
+    page.tab_ls.click()
+    page.email_input_form.send_keys(ls_no_valid)
+    page.password_input_form.send_keys(valid_password)
+    page.check_mark.click()
+    page.btn.click()
+    assert page.forgot_password.is_visible()
+    assert page.invalid_login_password.is_visible()
+
