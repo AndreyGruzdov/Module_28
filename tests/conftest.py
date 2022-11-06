dc87ab57ab716887c6240f6b656c87a8ad9e0af8
AndreyGruzdov <andrj169@gmail.com>
2022-11-06 09:33:48 +0500

    изменен путь к драйверу

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ def get_chrome_options():
 @pytest.fixture(scope='function')
 def web_browser(request, get_chrome_options):
     options = get_chrome_options
-    driver = webdriver.Chrome(executable_path='C:\SkillFactory\Driver/chromedriver.exe', options=options)
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=options)
     if request.cls is not None:
         request.cls.driver = driver
     driver.delete_all_cookies()
