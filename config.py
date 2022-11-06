2213fb4e628b2b28946f28df6c4994ce3f75b9a0
AndreyGruzdov <andrj169@gmail.com>
2022-11-06 09:47:04 +0500

    добавлена переменная, функция

--- config.py
+++ config.py
@@ -6,8 +6,10 @@ last_mame = 'Иванов'
 valid_email = 'tobeve1988@dmtubes.com'
 valid_password = '@cxLzXZBGE46gz7'
 valid_telephone = '+79522840568'
-no_valid_telephone = '+***********'#'+7*522840568'
+no_valid_telephone = '+***********'
 url_rostelecom = 'https://b2c.passport.rt.ru/'
+start_web = 'https://start.rt.ru/'
+
 url_cod = 'https://my.rt.ru/'
 url_email = "temp-mail.org/ru/"
 
@@ -20,6 +22,10 @@ def pass_gener():
         passw_random = ''.join([passw_random,random.choice(list(engl))])
 
     return passw_random.lower()
+
+def code():
+    dom = random.choice('1234567890')
+    return dom
 
 def email_gener():
     name_email = ''
